9168cc4de8a0a7fb823ce65457035f2f7e0178e3
Morest1124 <morestburawa05@gmail.com>
2025-08-06 19:17:12 +0200

    Added validation to a form

---
+++ Form-Creation-Validation/script.js
@@ -0,0 +1,51 @@
+addEventListener("DOMContentLoaded", (event) => {
+  const Form = document.getElementById("registration-form");
+
+  const feedbackDiv = document.getElementById("feedback");
+
+  const form = document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const isValid = true;
+    const messages = [console.error()];
+  });
+
+  const userName = document.getElementById("username");
+  const userNameError = document.getElementById("usernameError");
+  const email = document.getElementById("email");
+  const emailError = document.getElementById("emailError");
+  const password = document.getElementById("password");
+  const passwordError = document.getElementById("passwordError");
+
+  const userNameInput = userName.value.trim();
+  const emailInput = email.value.trim();
+  const passwordInput = password.value.trim();
+
+  let isValid = true;
+  let messages = [];
+
+  if (userName.length < 3) {
+    isValid = false;
+    messages = "Usename should be at least 3 characters long";
+  } else {
+    console.log(userName.innerText);
+  }
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput)) {
+    isValid = false;
+    messages = "Enter the correct form of email";
+  } else {
+    console.log(email.innerText);
+  }
+
+  if (password.length < 8) {
+    isValid = false;
+    messages = "You passwaord should be atleast 8 characters";
+  } else {
+    console.log(password.innerText);
+  }
+
+  if (feedbackDiv) {
+    isValid = true;
+    feedbackDiv.textContent = "Registration successful!";
+    feedbackDiv.style.color = "#28a745";
+  }
+});
