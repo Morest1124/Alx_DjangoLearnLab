18c60e007577ae9135cfe9cd0d4e165d351d780b
Morest1124 <morestburawa05@gmail.com>
2025-08-29 04:29:21 +0200

    setup admin page

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import models
 
 # Register your models here.
+
+@admin.register(models)
+class modelsAdmin(admin.ModelAdmin):
+    Book = ('title', 'author', 'publication_year')
+
+    
+    
+    
