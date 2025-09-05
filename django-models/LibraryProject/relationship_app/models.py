e153a7dfaecd679f4aceef3a6288356cd281939b
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-05 23:55:43 +0200

    Add str method

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,16 +4,31 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.name
+    
     
 class Book(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
+    
+    def __str__(self):
+        return self.title
+    
     
     
 class Library(models.Model):
     name = models.CharField(max_length=30)
     books = models.ManyToManyField(Book, related_name='library')
     
+    def __str__(self):
+        return self.name
+    
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=30)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="Librarian")
+    
+    def __str__(self):
+        return self.name
+    
