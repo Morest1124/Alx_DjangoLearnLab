09fb9eef873b71c24df5408a5fe537c6232ac288
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-06 00:11:51 +0200

    Fix library retrieval in get_librarian_for_library function

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -33,7 +33,7 @@ def get_librarian_for_library(library_name):
     Retrieves the librarian for a library.
     """
     try:
-        library = Library.objects.filter(name=library_name)
+        library = Library.objects.get(name=library_name)
         librarian = library.librarian
         print(f"\nLibrarian for {library.name} Library:")
         print(f"- {librarian.name}")
