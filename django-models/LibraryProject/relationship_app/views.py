6041fe857d5dee58cb5eac4802c18cd0c9a84b97
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-06 19:45:13 +0200

    Add import views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Author, Library
 from .models import Library
 # Create your views here.
