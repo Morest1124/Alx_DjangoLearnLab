15fff266999c0bc1cd51810828a7b244d667ec15
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-06 19:48:28 +0200

    Add DetailView import to urls.py for future use

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path, include
+from django.views.generic.detail import DetailView
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
