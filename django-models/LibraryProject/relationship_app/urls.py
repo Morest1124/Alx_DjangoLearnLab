4e132bafa2458a5a8bd05178ceebd4eadd2ce1f8
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-06 20:16:17 +0200

    Rename 'book_list' to 'list_books' in urlpatterns for consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import book_list
 from .views import list_books
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
+    path('books/', views.book_list, name='list_books'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
