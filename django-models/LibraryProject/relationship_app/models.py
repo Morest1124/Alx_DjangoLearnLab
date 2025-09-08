8c646dad5da66f35cc41fa53c2072eb16947f8ff
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-08 23:21:49 +0200

    imported missing modules and models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,11 +3,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Library
-from .models import Book
-from .forms import BookForm
-
-
 
 # Create your models here.
 class Author(models.Model):
@@ -21,7 +16,12 @@ class Book(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     
-    
+    class Meta:
+        permissions = (
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        )
     
     def __str__(self):
         return self.title
