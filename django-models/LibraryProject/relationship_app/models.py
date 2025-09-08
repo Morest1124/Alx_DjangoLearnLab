d3a21bcc1fd23575331dc405a63b47eca0463b74
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-08 23:30:44 +0200

    fixing form.py in models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -15,6 +15,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    published_date = models.DateField(null=True, blank=True)
     
     class Meta:
         permissions = (
