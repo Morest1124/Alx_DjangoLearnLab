58c7b99570895796e845c6c613f41aa118520eb0
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-13 09:28:52 +0200

    Add form to bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
 
 def book_list(request):
     books = Book.objects.all()
@@ -8,26 +9,26 @@ def book_list(request):
 
 @permission_required('bookshelf.add_book', raise_exception=True)
 def add_book(request):
-    # This is a placeholder. In a real app, you'd use a form.
     if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        publication_year = request.POST.get('publication_year')
-        Book.objects.create(title=title, author=author, publication_year=publication_year)
-        return redirect('book_list')
-    return render(request, 'bookshelf/add_book.html')
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
 
 @permission_required('bookshelf.change_book', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
-    # This is a placeholder. In a real app, you'd use a form.
     if request.method == 'POST':
-        book.title = request.POST.get('title')
-        book.author = request.POST.get('author')
-        book.publication_year = request.POST.get('publication_year')
-        book.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/edit_book.html', {'book': book})
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
 @permission_required('bookshelf.delete_book', raise_exception=True)
 def delete_book(request, pk):
