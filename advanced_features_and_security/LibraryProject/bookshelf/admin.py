aa2ba933928dc54efa5c150201344395f4a57243
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-18 10:16:19 +0200

    etting up a new Django project from scratch, specifically for building APIs

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Book, CustomUser
+from .models import Book
 
 # Register your models here.
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
+    list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author', 'publication_year')
 
 class CustomUserAdmin(UserAdmin):
