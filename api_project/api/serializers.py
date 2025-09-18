0011d1ea56c4f71d0b22daa38dd859b7e638959d
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-18 11:55:10 +0200

    feat: Add BookList API endpoint
    This commit introduces the foundational API endpoint for the Book model. It establishes a read-only view that allows clients to retrieve a list of all books from the database.
    
    The changes include:
    
    Defining a BookList view in api/views.py using ListAPIView, which is configured to use the BookSerializer and the Book model's queryset.
    
    Creating a new URL pattern in api/urls.py that maps the /books/ path to the new BookList view.

---
+++ api_project/api/serializers.py
@@ -0,0 +1,18 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    """A serializer for the book model.
+    
+    automatically handles serializations for all fields as set field = '__all__'
+    """
+    class Meta:
+        #book model
+        model = Book
+        
+        #Include all fields
+        field = '__all__'
+        
+    
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from django.urls import path
+from .models import Book
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    """API view listing all books
+    
+    provide a readonly endpoint"""
+    #set all queryset of all objects to be retrived
+    queryset = Book.objects.all()
+    
+    serializer_class =BookSerializer # tells how to serialize
