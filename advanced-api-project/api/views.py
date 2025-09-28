f2cdb168b3f13e97830992c3702d915795950505
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-29 01:01:51 +0200

    Corrected imports

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,9 @@
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import generics
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 # A ListView for retrieving all books.
