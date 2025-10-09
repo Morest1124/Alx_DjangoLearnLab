32c47790c1bb348aa53870e2c45930c3adc50530
Morest1124 <morest1124burawa2005@gmail.com>
2025-10-09 12:40:03 +0200

    fix: Correct typo in CustomUser model (boi -> bio)

--- social_media_api/social_media_api/accounts/models.py
+++ social_media_api/social_media_api/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    boi = models.CharField(blank= True)
+    bio = models.CharField(blank= True)
     profile_picture = models.ImageField(upload_to='profile/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='Following')
     followering = models.ManyToManyField('self', symmetrical=False, related_name='Follower')
